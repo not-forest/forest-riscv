8fecb89d65b5b6a39b0fb3936dfebfee609452a9
Notforest <notforest69@gmail.com>
2024-08-22 22:11:51 +0200

    Initialized the Quartus environment with several test hardware symbols.

---
+++ p_counter_inst.vhd
@@ -0,0 +1,6 @@
+p_counter_inst : p_counter PORT MAP (
+		clock	 => clock_sig,
+		data	 => data_sig,
+		sload	 => sload_sig,
+		q	 => q_sig
+	);
---
+++ p_rom_inst.vhd
@@ -0,0 +1,5 @@
+p_rom_inst : p_rom PORT MAP (
+		address	 => address_sig,
+		clock	 => clock_sig,
+		q	 => q_sig
+	);
