26a04a3fc6e1c7ea28f2a6b45e9b47291d91a76a
Notforest <notforest69@gmail.com>
2024-08-26 04:23:56 +0200

    Added more mutexes.

---
+++ im_mux_inst.vhd
@@ -0,0 +1,6 @@
+im_mux_inst : im_mux PORT MAP (
+		data0x	 => data0x_sig,
+		data1x	 => data1x_sig,
+		sel	 => sel_sig,
+		result	 => result_sig
+	);
---
+++ pc_mux_inst.vhd
@@ -0,0 +1,6 @@
+pc_mux_inst : pc_mux PORT MAP (
+		data0x	 => data0x_sig,
+		data1x	 => data1x_sig,
+		sel	 => sel_sig,
+		result	 => result_sig
+	);
