b84a0b0aa3e6c9bf6abf30b8faece0874d33197e
Notforest <notforest69@gmail.com>
2024-08-26 04:27:53 +0200

    Moving from 16-bit to 32-bit counter.

--- p_counter.vhd
+++ p_counter.vhd
@@ -44,16 +44,17 @@ ENTITY p_counter IS
 	PORT
 	(
 		clock		: IN STD_LOGIC ;
-		data		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		data		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		sclr		: IN STD_LOGIC ;
 		sload		: IN STD_LOGIC ;
-		q		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+		q		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 END p_counter;
 
 
 ARCHITECTURE SYN OF p_counter IS
 
-	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (15 DOWNTO 0);
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (31 DOWNTO 0);
 
 
 
@@ -66,25 +67,27 @@ ARCHITECTURE SYN OF p_counter IS
 	);
 	PORT (
 			clock	: IN STD_LOGIC ;
-			data	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			data	: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+			sclr	: IN STD_LOGIC ;
 			sload	: IN STD_LOGIC ;
-			q	: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+			q	: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 	END COMPONENT;
 
 BEGIN
-	q    <= sub_wire0(15 DOWNTO 0);
+	q    <= sub_wire0(31 DOWNTO 0);
 
 	LPM_COUNTER_component : LPM_COUNTER
 	GENERIC MAP (
 		lpm_direction => "UP",
 		lpm_port_updown => "PORT_UNUSED",
 		lpm_type => "LPM_COUNTER",
-		lpm_width => 16
+		lpm_width => 32
 	)
 	PORT MAP (
 		clock => clock,
 		data => data,
+		sclr => sclr,
 		sload => sload,
 		q => sub_wire0
 	);
@@ -108,26 +111,28 @@ END SYN;
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 -- Retrieval info: PRIVATE: ModulusCounter NUMERIC "0"
 -- Retrieval info: PRIVATE: ModulusValue NUMERIC "0"
--- Retrieval info: PRIVATE: SCLR NUMERIC "0"
+-- Retrieval info: PRIVATE: SCLR NUMERIC "1"
 -- Retrieval info: PRIVATE: SLOAD NUMERIC "1"
 -- Retrieval info: PRIVATE: SSET NUMERIC "0"
 -- Retrieval info: PRIVATE: SSET_ALL1 NUMERIC "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
--- Retrieval info: PRIVATE: nBit NUMERIC "16"
+-- Retrieval info: PRIVATE: nBit NUMERIC "32"
 -- Retrieval info: PRIVATE: new_diagram STRING "1"
 -- Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 -- Retrieval info: CONSTANT: LPM_DIRECTION STRING "UP"
 -- Retrieval info: CONSTANT: LPM_PORT_UPDOWN STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_COUNTER"
--- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "16"
+-- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "32"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT NODEFVAL "clock"
--- Retrieval info: USED_PORT: data 0 0 16 0 INPUT NODEFVAL "data[15..0]"
--- Retrieval info: USED_PORT: q 0 0 16 0 OUTPUT NODEFVAL "q[15..0]"
+-- Retrieval info: USED_PORT: data 0 0 32 0 INPUT NODEFVAL "data[31..0]"
+-- Retrieval info: USED_PORT: q 0 0 32 0 OUTPUT NODEFVAL "q[31..0]"
+-- Retrieval info: USED_PORT: sclr 0 0 0 0 INPUT NODEFVAL "sclr"
 -- Retrieval info: USED_PORT: sload 0 0 0 0 INPUT NODEFVAL "sload"
 -- Retrieval info: CONNECT: @clock 0 0 0 0 clock 0 0 0 0
--- Retrieval info: CONNECT: @data 0 0 16 0 data 0 0 16 0
+-- Retrieval info: CONNECT: @data 0 0 32 0 data 0 0 32 0
+-- Retrieval info: CONNECT: @sclr 0 0 0 0 sclr 0 0 0 0
 -- Retrieval info: CONNECT: @sload 0 0 0 0 sload 0 0 0 0
--- Retrieval info: CONNECT: q 0 0 16 0 @q 0 0 16 0
+-- Retrieval info: CONNECT: q 0 0 32 0 @q 0 0 32 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL p_counter.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL p_counter.inc TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL p_counter.cmp TRUE
--- p_counter_inst.vhd
+++ p_counter_inst.vhd
@@ -1,6 +1,7 @@
 p_counter_inst : p_counter PORT MAP (
 		clock	 => clock_sig,
 		data	 => data_sig,
+		sclr	 => sclr_sig,
 		sload	 => sload_sig,
 		q	 => q_sig
 	);
