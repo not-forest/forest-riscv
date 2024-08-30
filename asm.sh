3ee1eff1a22e598a873126a9acde48eaacb72d0e
Notforest <notforest69@gmail.com>
2024-08-30 15:43:42 +0200

    Fixed the bash script for compiling the program.

--- asm.sh
+++ asm.sh
@@ -8,21 +8,19 @@ BINARY_FILE="asm/program.bin"
 INTEL_HEX_FILE="asm/program.hex"
 
 # Assemble the assembly file to object file
-riscv32-unknown-elf-as -o $OBJECT_FILE $ASSEMBLY_FILE
+riscv32-unknown-linux-gnu-as -o $OBJECT_FILE $ASSEMBLY_FILE
 
 # Link the object file to create an executable
-riscv32-unknown-elf-ld -o $EXECUTABLE_FILE $OBJECT_FILE
+riscv32-unknown-linux-gnu-ld -o $EXECUTABLE_FILE $OBJECT_FILE
 
 # Convert the executable to a binary file
-riscv32-unknown-elf-objcopy -O binary $EXECUTABLE_FILE $BINARY_FILE
+riscv32-unknown-linux-gnu-objcopy -O binary $EXECUTABLE_FILE $BINARY_FILE
 
 # Convert the binary file to Intel Hex format
-srec_cat $BINARY_FILE -binary -intel $INTEL_HEX_FILE
-
-# Optionally, open the Intel Hex file in GHex for verification
-ghex $INTEL_HEX_FILE
+srec_cat $BINARY_FILE -binary -o $INTEL_HEX_FILE -Intel
 
 # Clean up intermediate files if needed
 rm $OBJECT_FILE $EXECUTABLE_FILE $BINARY_FILE
 
 echo "Conversion completed. Intel Hex file: $INTEL_HEX_FILE"
+
