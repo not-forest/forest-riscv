4af8285c643850b17bdcfe6a5b5e8b6298b072c5
Notforest <notforest69@gmail.com>
2024-09-06 18:35:41 +0200

    Fixed the toolchain for generating assembly. Fixed srec_cat generating big-endian code.

--- asm.sh
+++ asm.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-# Define file names
-ASSEMBLY_FILE="asm/program.s"
-OBJECT_FILE="asm/program.o"
-EXECUTABLE_FILE="asm/program.elf"
-BINARY_FILE="asm/program.bin"
-INTEL_HEX_FILE="asm/program.hex"
+DIR=$(dirname "$(realpath "$0")")
+ASBDIR="$DIR/asm"
 
-# Assemble the assembly file to object file
-riscv32-unknown-linux-gnu-as -o $OBJECT_FILE $ASSEMBLY_FILE
+# Defines file names
+ASSEMBLY_FILE="$ASBDIR/program.s"
+OBJECT_FILE="$ASBDIR/program.o"
+EXECUTABLE_FILE="$ASBDIR/program.elf"
+BINARY_FILE="$ASBDIR/program.bin"
+INTEL_HEX_FILE="$ASBDIR/program.hex"
 
-# Link the object file to create an executable
-riscv32-unknown-linux-gnu-ld -o $EXECUTABLE_FILE $OBJECT_FILE
+# Assembles the assembly file to object file (base RV32I instruction set.)
+riscv32-unknown-elf-as -march=rv32i -o $OBJECT_FILE $ASSEMBLY_FILE
+riscv32-unknown-elf-ld -o $EXECUTABLE_FILE $OBJECT_FILE -Ttext=0x0 --entry=_start
+riscv32-unknown-elf-objcopy -O binary $EXECUTABLE_FILE $BINARY_FILE
 
-# Convert the executable to a binary file
-riscv32-unknown-linux-gnu-objcopy -O binary $EXECUTABLE_FILE $BINARY_FILE
-
-# Convert the binary file to Intel Hex format
-srec_cat $BINARY_FILE -binary -o $INTEL_HEX_FILE -Intel
+# Convert the binary file to Intel Hex format (little-endian)
+srec_cat $BINARY_FILE -binary -byte-swap 4 -o $INTEL_HEX_FILE -Intel
 
 # Clean up intermediate files if needed
 rm $OBJECT_FILE $EXECUTABLE_FILE $BINARY_FILE
 
 echo "Conversion completed. Intel Hex file: $INTEL_HEX_FILE"
-
