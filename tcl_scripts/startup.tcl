50868b57b1f99b301b4cf2e4b971be92da4edf33
Notforest <notforest69@gmail.com>
2024-09-06 18:36:41 +0200

    Fixed proper tcl exit commands. Added more docs for new script instructions. Included the instruction module.

--- tcl_scripts/startup.tcl
+++ tcl_scripts/startup.tcl
@@ -9,7 +9,7 @@ puts "Initializing Forest-RiscV CPU debug environment..."
 set SERVICE_PATHS [get_service_paths master]
 if { [llength $SERVICE_PATHS] == 0 } {
     puts "Error: No service paths found. Ensure the design is loaded correctly and the JTAG connection is established."
-    exit 1
+    exit 2
 }
 puts "Obtaining service paths. \[DONE\]"
 
@@ -17,29 +17,33 @@ puts "Obtaining service paths. \[DONE\]"
 set MASTER_SERVICE_PATH [lindex $SERVICE_PATHS 0]
 if { $MASTER_SERVICE_PATH == "" } {
     puts "Error: Could not retrieve the master service path."
-    exit 1
+    exit 2
 }
 puts "Obtaining the MM master service path. \[DONE\]"
 
 # Claim the master.
 if { [catch {set CLAIM_PATH [claim_service master $MASTER_SERVICE_PATH mylib]} result] } {
     puts "Error: Failed to claim the master service: $result"
-    exit 1
+    exit 2
 }
 
 if { $CLAIM_PATH == "" } {
     puts "Error: Claiming the master service failed. The returned claim path is empty."
-    exit 1
+    exit 2
 }
 puts "Claiming the MM master service. \[DONE\]"
 
+# Memory mapped PIO addresses.
 set PIO_CLK             0x00
 set PIO_REG_SELECT      0x10
 set PIO_REG_BUS         0x20
 set PIO_PC              0x30
+set PIO_INSTR           0x40
+set PIO_CODE            0x50
 
 source ./tcl_scripts/clock.tcl
 source ./tcl_scripts/regs.tcl
+source ./tcl_scripts/instruction.tcl
 
 puts "Initialization \[DONE\]"
 
@@ -54,8 +58,19 @@ proc help {} {
     append instructions "2. Print all registers (x0...x31) and the PC register:\n"
     append instructions "   Usage: read_regs\n"
 
-    append instructions "3. Do one step and then print all registers status.:\n"
-    append instructions "   Usage: step\n"
+    append instructions "3. Obtain value of one of 32 general purpoose registers (x0...x31):\n"
+    append instructions "   Usage: read_reg \[reg_id\]\n"
+
+    append instructions "4. Obtain value of the PC register:\n"
+    append instructions "   Usage: read_pc\n"
+    
+    append instructions "5. Dissasembles the oncoming instruction obtained from the program memory. Because
+                    CPU is pipelined, the cpu does fetch 4 sequential instructions during it's whole instruction cycle:\n"
+    append instructions "   Usage: dissasemble\n"
+
+    append instructions "6. Do n CPU steps and then print all of the above:\n"
+    append instructions "   Usage: step \[n\]\n"
+    append instructions "   - \[n\] (optional): Number of cycles to tick the CPU. Default is 1 cycle.\n"
     
     append instructions "-----------------------------------------\n"
     
@@ -63,10 +78,11 @@ proc help {} {
     puts $instructions
 }
 
-# Performs one CPU step and provides the current register's status.
-proc step {} {
-    tick
+# Performs n CPU steps and provides the whole CPU status.
+proc step {{n 1}} {
+    tick $n
     read_regs
+    disassemble
 }
 
 help
