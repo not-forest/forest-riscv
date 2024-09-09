5b042fcf684fe182234f3d4ade92d51f61f837da
Notforest <notforest69@gmail.com>
2024-09-09 20:55:13 +0200

    Changed to async clear.

--- pp_counter.vhd
+++ pp_counter.vhd
@@ -43,8 +43,8 @@ USE lpm.all;
 ENTITY pp_counter IS
 	PORT
 	(
+		aclr		: IN STD_LOGIC ;
 		clock		: IN STD_LOGIC ;
-		sclr		: IN STD_LOGIC ;
 		q		: OUT STD_LOGIC_VECTOR (1 DOWNTO 0)
 	);
 END pp_counter;
@@ -64,8 +64,8 @@ ARCHITECTURE SYN OF pp_counter IS
 		lpm_width		: NATURAL
 	);
 	PORT (
+			aclr	: IN STD_LOGIC ;
 			clock	: IN STD_LOGIC ;
-			sclr	: IN STD_LOGIC ;
 			q	: OUT STD_LOGIC_VECTOR (1 DOWNTO 0)
 	);
 	END COMPONENT;
@@ -81,8 +81,8 @@ BEGIN
 		lpm_width => 2
 	)
 	PORT MAP (
+		aclr => aclr,
 		clock => clock,
-		sclr => sclr,
 		q => sub_wire0
 	);
 
@@ -93,7 +93,7 @@ END SYN;
 -- ============================================================
 -- CNX file retrieval info
 -- ============================================================
--- Retrieval info: PRIVATE: ACLR NUMERIC "0"
+-- Retrieval info: PRIVATE: ACLR NUMERIC "1"
 -- Retrieval info: PRIVATE: ALOAD NUMERIC "0"
 -- Retrieval info: PRIVATE: ASET NUMERIC "0"
 -- Retrieval info: PRIVATE: ASET_ALL1 NUMERIC "1"
@@ -105,7 +105,7 @@ END SYN;
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 -- Retrieval info: PRIVATE: ModulusCounter NUMERIC "0"
 -- Retrieval info: PRIVATE: ModulusValue NUMERIC "0"
--- Retrieval info: PRIVATE: SCLR NUMERIC "1"
+-- Retrieval info: PRIVATE: SCLR NUMERIC "0"
 -- Retrieval info: PRIVATE: SLOAD NUMERIC "0"
 -- Retrieval info: PRIVATE: SSET NUMERIC "0"
 -- Retrieval info: PRIVATE: SSET_ALL1 NUMERIC "1"
@@ -117,11 +117,11 @@ END SYN;
 -- Retrieval info: CONSTANT: LPM_PORT_UPDOWN STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_COUNTER"
 -- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "2"
+-- Retrieval info: USED_PORT: aclr 0 0 0 0 INPUT NODEFVAL "aclr"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT NODEFVAL "clock"
 -- Retrieval info: USED_PORT: q 0 0 2 0 OUTPUT NODEFVAL "q[1..0]"
--- Retrieval info: USED_PORT: sclr 0 0 0 0 INPUT NODEFVAL "sclr"
+-- Retrieval info: CONNECT: @aclr 0 0 0 0 aclr 0 0 0 0
 -- Retrieval info: CONNECT: @clock 0 0 0 0 clock 0 0 0 0
--- Retrieval info: CONNECT: @sclr 0 0 0 0 sclr 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 2 0 @q 0 0 2 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL pp_counter.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL pp_counter.inc TRUE
