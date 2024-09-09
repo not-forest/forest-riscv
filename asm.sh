6303edc15e30821adad08950cc8469a27ce90776
Notforest <notforest69@gmail.com>
2024-09-09 21:04:50 +0200

    The program to compile could be chosen by changing the script variable. TODO! in fist non-prototyped version use Makefile.

--- asm.sh
+++ asm.sh
@@ -2,13 +2,14 @@
 
 DIR=$(dirname "$(realpath "$0")")
 ASBDIR="$DIR/asm"
+PROGRAM="branch"
 
 # Defines file names
-ASSEMBLY_FILE="$ASBDIR/program.s"
-OBJECT_FILE="$ASBDIR/program.o"
-EXECUTABLE_FILE="$ASBDIR/program.elf"
-BINARY_FILE="$ASBDIR/program.bin"
-INTEL_HEX_FILE="$ASBDIR/program.hex"
+ASSEMBLY_FILE="$ASBDIR/$PROGRAM.s"
+OBJECT_FILE="$ASBDIR/$PROGRAM.o"
+EXECUTABLE_FILE="$ASBDIR/$PROGRAM.elf"
+BINARY_FILE="$ASBDIR/$PROGRAM.bin"
+INTEL_HEX_FILE="$ASBDIR/$PROGRAM.hex"
 
 # Assembles the assembly file to object file (base RV32I instruction set.)
 riscv32-unknown-elf-as -march=rv32i -o $OBJECT_FILE $ASSEMBLY_FILE
